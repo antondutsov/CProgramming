a9bc956a379945b06203a412d0e118a972ca52e9
antondutsov <anton@dutsov.com>
2021-01-14 08:54:52 +0200

    added 'week_3' folder

---
+++ week_03/20210113_6.c
@@ -0,0 +1,21 @@
+/*
+6. Дефинирайте променливи със стойност -127, 255, 6 237 498, 4 294 967 292, -9 000 000 000 000 775 845.
+Изведете всяка променлива на екрана със printf();
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    char valueOne = -127;
+    unsigned char valueTwo = 255;
+    int valueThree = 6237498;
+    long int valueFour = -9000000000000775845;
+
+    printf("The 'signed char' value is: %d \n", valueOne);
+    printf("The 'unsigned char' value is: %d \n", valueTwo);
+    printf("The 'signed int' value is: %ld \n", valueThree);
+    printf("The 'long int' value is: %ld \n", valueFour);
+
+return 0;
+}
---
+++ week_03/20210113_9.c
@@ -0,0 +1,28 @@
+/*
+9. Дефинирайте променливи със стойност
+4.9876543, 7.123456789012345678890, 18 398 458 438 583 853.28, 18 398 458 438
+583 853.39875937284928422.
+Изведете всяка променлива на екрана със printf();
+*/
+
+
+#include <stdio.h>
+//#include <float.h>
+//#include <quadmath.h>
+
+int main(){
+
+    double valueOne = 4.9876543;
+    double valueTwo = 7.123456789012345678890;
+    long double valueThree = 18398458438583853.28;
+    long double valueFour = 18398458438583853.39875937284928422;
+
+    printf("The first value is: %.7f \n", valueOne);
+    printf("The second value is: %.21f \n", valueTwo);
+    printf("The third value is: %.2Lf \n", valueThree);
+    printf("The fourth value is: %Lf \n", valueFour);
+
+return 0;
+}
+
+
