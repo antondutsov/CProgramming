087c3b31514dfc7f2f86977958c0716ff42df18e
antondutsov <anton@dutsov.com>
2021-02-09 15:33:53 +0200

    added week_06

--- week_06/20210208/20210208_04.c
+++ week_06/20210208/20210208_04.c
@@ -3,14 +3,31 @@
 стойност на N),напишете функция, която изчислява средната стойност на
 елементите в масива, като я връща като double float.
 */
+#include<stdio.h>
 
-#include <stdio.h>
+double calcAverage(double alpha[], int lenght);
 
-int main(){
+int main(void) {
+	int n, i;
+	printf("Моля въведете от колко елемента ще се състои масивът: \n");
+	scanf("%d", &n);
+	double alpha[n];
 
+	for (i = 0; i < n; i++) {
+		printf("Въведете стойност за [%d]: ", i);
+		scanf("%lf", &alpha[i]);
+	}
 
+	printf("Средно-аритметичната стойност е %.2lf", calcAverage(alpha, n));
 
+	return 0;
+}
 
+double calcAverage(double alpha[], int length) {
+	double rez = 0.0;
+	for (int i = 0; i < length; i++) {
+		rez += alpha[i];
+	}
 
-return 0;
+	return rez / length;
 }
--- week_06/20210208/20210208_06.c
+++ week_06/20210208/20210208_06.c
@@ -3,13 +3,27 @@
 най-голямото от тях.
 */
 
-#include <stdio.h>
+#include<stdio.h>
+#include <limits.h>
 
-int main(){
+int largestElement(int *a, int size);
 
+int main(void) {
+	int alpha[7] = {40, 27, 1, 3, 6, 24, -8};
 
+	printf("Най-голямата стойност от масива е: %d \n", largestElement(alpha, 7));
 
+	return 0;
+}
 
+int largestElement(int *a, int size) {
+	int largest;
+	largest = INT_MIN;
 
-return 0;
+	for (int i = 0; i < size; i++) {
+		if (a[i] > largest)
+			largest = a[i];
+	}
+
+	return largest;
 }
