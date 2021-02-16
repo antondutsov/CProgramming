9013285417a2eae4a7448176cefc7cb32042b4ba
antondutsov <anton@dutsov.com>
2021-02-16 19:21:18 +0200

    modified task11.c

--- week_04/20210125/20210125_11.c
+++ week_04/20210125/20210125_11.c
@@ -5,3 +5,34 @@ i = 0, j = strlen(s) – 1; i < j; i++, j-- , за да обърнете стр�
 */
 
 
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char s[])
+{
+    for (int i = 0, j = (strlen(s) - 1); i < j; i++, j--)
+    {
+        char temp = s[j];
+        s[j] = s[i];
+        s[i] = temp;
+    }
+    printf("%s\n", s);
+}
+
+int main()
+{
+    char c = 0;
+    char s[100];
+    while (c != EOF)
+    {
+        c = getchar();
+        strncat(s, &c, 1);
+        if (c == '\n')
+        {
+            reverse(s);
+            *s = '\0';
+        }
+    }
+
+return 0;
+}
